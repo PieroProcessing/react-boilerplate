2545002a6ac715ff85f93dc475e538b94b311bd1
PieroProcessing <piero.dauber@gmail.com>
2020-12-28 18:50:05 +0100

    add new service

--- testing-console/src/services/index.tsx
+++ testing-console/src/services/index.tsx
@@ -1,4 +1,5 @@
 import _fetch from './fetch.service';
 import ENDPOINT from './endpoints.service';
+import setUrlQeueryParams from './serch-params.service';
 
-export { _fetch, ENDPOINT };
+export { _fetch, ENDPOINT, setUrlQeueryParams };
