283018ab22d00d2e15451b670dfe8becc93426e5
PieroProcessing <piero.dauber@gmail.com>
2020-12-28 18:51:35 +0100

    simple search

--- testing-console/src/pages/react-query/use-query.simple-list.tsx
+++ testing-console/src/pages/react-query/use-query.simple-list.tsx
@@ -2,6 +2,7 @@ const SimpleRequest = (): JSX.Element => {
   const handleChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
     console.log('🚀 ~ file: App.tsx ~ line 42 ~ handleChange ~ handleChange', value);
   };
+
   return (
     <>
       <label className="d-flex flex-column" htmlFor="pokemon">
