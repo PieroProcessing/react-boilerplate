486984bd05fbeb591670bef3fe5e649df95dc144
PieroProcessing <piero.dauber@gmail.com>
2020-12-29 11:12:59 +0100

    refined pagination

--- testing-console/src/components/pagination.component.tsx
+++ testing-console/src/components/pagination.component.tsx
@@ -4,25 +4,26 @@ interface PaginationModel {
   page: number;
   handlePagination: ({ currentTarget }: React.MouseEvent<HTMLElement>) => void;
   data: Response | undefined;
+  offset: number;
 }
-const Pagination = ({ page, handlePagination, data }: PaginationModel): JSX.Element => {
+const Pagination = ({ page, handlePagination, offset, data }: PaginationModel): JSX.Element => {
   return (
-    <nav aria-label="Page navigation example">
-      <ul className="pagination justify-content-center">
+    <nav aria-label="row">
+      <ul className="pagination justify-content-between">
         <li className={`page-item ${page === 0 ? 'disabled' : ''}`}>
-          <button type="button" className="page-link" onClick={handlePagination} value={-200} disabled={page === 0}>
+          <button type="button" className="page-link" onClick={handlePagination} value={-offset} disabled={page === 0}>
             Previous
           </button>
         </li>
 
         <li className="page-item">
-          <button type="button" className="page-link">
-            {page}
-          </button>
+          <div className="d-flex align-center text-primary">
+            {page} / {data?.count}
+          </div>
         </li>
 
         <li className={`page-item ${!data?.next ? 'disabled' : ''}`}>
-          <button type="button" className="page-link" onClick={handlePagination} value={+200} disabled={!data?.next}>
+          <button type="button" className="page-link" onClick={handlePagination} value={offset} disabled={!data?.next}>
             Next
           </button>
         </li>
