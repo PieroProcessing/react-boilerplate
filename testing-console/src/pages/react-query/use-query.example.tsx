864961a144b3e765d90728919d0a8268ee711060
PieroProcessing <piero.dauber@gmail.com>
2020-12-30 14:42:14 +0100

    bugfix button selection component

--- testing-console/src/pages/react-query/use-query.example.tsx
+++ testing-console/src/pages/react-query/use-query.example.tsx
@@ -1,31 +1,28 @@
-import { useEffect, useState } from 'react';
-import { QueryFunctionContext, useQuery, useQueryClient } from 'react-query';
-import { Response } from '../../models';
-import { _fetch, ENDPOINT } from '../../services';
-import SimpleRequest from './use-query.simple-list';
+import { useState } from 'react';
+import PostRequest from './mutations.example';
 import QueryPaginated from './use-query.paginated';
 
 const UseQueryExample = (): JSX.Element => {
-  const [example, setExample] = useState<boolean>(false);
+  const [example, setExample] = useState<boolean>(true);
 
   const handleSelect = ({ currentTarget }: React.MouseEvent<HTMLElement>): void => {
-    setExample(Boolean(currentTarget.getAttribute('value')));
+    setExample(currentTarget.getAttribute('value') === 'true');
   };
   const handleClick = ({ currentTarget }: React.MouseEvent<HTMLElement>): void => {
-    setExample(Boolean(currentTarget.getAttribute('value')));
+    setExample(currentTarget.getAttribute('data-value') === 'true');
   };
 
   return (
     <>
       <header className="d-inline-flex align-items-center justify-content-around w-100 py-3 border-bottom">
-        <div className="btn btn-secondary" onClick={handleClick} data-value={false} aria-hidden="true">
+        <div className="btn btn-secondary" onClick={handleClick} data-value="false" aria-hidden="true">
           UseQueryPagination Example
         </div>
         <button type="button" className="btn btn-default btn-primary" onClick={handleSelect} value="true">
-          UseQuery Example
+          Mutation Example
         </button>
       </header>
-      {example ? <SimpleRequest /> : <QueryPaginated />}
+      {example ? <PostRequest /> : <QueryPaginated />}
     </>
   );
 };
