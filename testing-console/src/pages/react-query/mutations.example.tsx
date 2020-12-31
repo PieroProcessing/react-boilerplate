5b7eeaef33e28e4473b500bf95878864753c9a74
PieroProcessing <piero.dauber@gmail.com>
2020-12-31 15:21:09 +0100

    optimaztion for mutation example

--- testing-console/src/pages/react-query/mutations.example.tsx
+++ testing-console/src/pages/react-query/mutations.example.tsx
@@ -1,23 +1,11 @@
 import { useEffect, useState } from 'react';
-import {
-  MutateOptions,
-  MutationFunction,
-  QueryFunctionContext,
-  QueryKey,
-  UseMutateFunction,
-  useMutation,
-  UseMutationResult,
-  useQuery,
-  useQueryClient,
-} from 'react-query';
-import { SetDataOptions } from 'react-query/types/core/query';
-import { Updater } from 'react-query/types/core/utils';
+import { useQueryClient } from 'react-query';
 import Pagination from '../../components/pagination.component';
-import { PostParams, PostResponseModel, RequestResponse } from '../../models';
 import { RequestData } from '../../models/request';
-import { ENDPOINT, setUrlQeueryParams, _fetch, _post } from '../../services';
+import { ENDPOINT, usePost, useList } from '../../services';
 
 const offset = 1;
+const queryKeyPostRequest = 'paginated_request_list';
 const obj = {
   name: 'morpheus',
   job: 'leader',
@@ -29,48 +17,17 @@ const obj = {
 };
 const PostRequest = (): JSX.Element => {
   const queryClient = useQueryClient();
-  // console.log('🚀 ~ file: mutations.example.tsx ~ line 31 ~ queryClient', queryClient);
   const [page, setPage] = useState<number>(1);
-  const [items, setItems] = useState<RequestData[]>(); // for demostrations purpose only
 
-  const { data, isLoading, isError, isSuccess, isFetching } = useQuery<RequestResponse, QueryFunctionContext>(
-    ['paginated_request_list', { url: `${ENDPOINT.requestList}?${setUrlQeueryParams({ page: String(page), per_page: String(12) }).toString()}` }],
-    _fetch,
-    {},
-  );
-  const handleUpdater = (input: RequestResponse[] | undefined): void => {
-    // console.log('🚀 ~ file: mutations.example.tsx ~ line 36 ~ queryClient.setQueryData ~ oldData', input);
-  };
-  const { mutate } = useMutation(_post, {
-    onMutate: async (newData) => {
-      await queryClient.cancelQueries('paginated_request_list');
-      const snapshot = queryClient.getQueryData<RequestResponse>('paginated_request_list');
-      if (snapshot) {
-        const newSnapshot = {
-          ...snapshot,
-          data: [...snapshot.data, newData.body],
-        };
-        queryClient.setQueryData<RequestResponse>('paginated_request_list', newSnapshot);
-        setItems(newSnapshot.data); // for demostrations purpose only
-        return newSnapshot;
-      }
-      return snapshot;
-    },
-    // If the mutation fails, use the context returned from onMutate to roll back
-    onError: (err, variables, context) => {
-      console.log('🚀 ~ file: mutations.example.tsx ~ line 61 ~ err, variables, context', err, variables, context);
-    },
-    // Always refetch after error or success:
-    onSettled: async () => {
-      // await queryClient.refetchQueries('paginated_request_list');
-    },
+  const list = useList(queryKeyPostRequest, ENDPOINT.requestList, {
+    page: String(page),
+    per_page: String(12),
   });
+  const { mutate } = usePost(queryClient, queryKeyPostRequest);
 
-  // console.log('🚀 ~ file: mutations.example.tsx ~ line 47 ~ mutate', mutate);
   useEffect((): (() => void) => {
-    setItems(data?.data);
-    return (): Promise<void> => queryClient.cancelQueries('paginated_request_list'); /// check if necessary and dont cache data on component destroy;
-  }, [data, queryClient]);
+    return (): Promise<void> => queryClient.cancelQueries(queryKeyPostRequest); /// check if necessary and dont cache data on component destroy;
+  }, [queryClient]);
   const handleChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
     // console.log('🚀 ~ file: App.tsx ~ line 42 ~ handleChange ~ handleChange', value);
   };
@@ -84,9 +41,7 @@ const PostRequest = (): JSX.Element => {
       mutate({ url: ENDPOINT.requestList, body: JSON.parse(newData) as RequestData });
     }
   };
-  const handleRedirect = ({ currentTarget }: React.MouseEvent<HTMLElement>): void => {
-    // console.log('🚀 ~ file: use-query.example.tsx ~ line 34 ~ handleRedirect ~ handleRedirect', currentTarget.getAttribute('data-value'));
-  };
+
   return (
     <>
       <section className="p-3">
@@ -101,10 +56,10 @@ const PostRequest = (): JSX.Element => {
         </form>
       </section>
       <section className="grid-wrapper">
-        {(isLoading || isFetching) && <div className="text-warning">data loading....</div>}
-        {isError && <div className="text-danger">ops... please try later...</div>}
-        {isSuccess && items?.length ? (
-          items?.map((user) => (
+        {(list.isLoading || list.isFetching) && <div className="text-warning">data loading....</div>}
+        {list.isError && <div className="text-danger">ops... please try later...</div>}
+        {list.isSuccess && list.data && list.data.data.length ? (
+          list.data.data.map((user) => (
             <div className="card" key={user.id}>
               <img src={user.avatar} className="card-img-top" alt="avatar" />
               <div className="card-body">
@@ -125,9 +80,9 @@ const PostRequest = (): JSX.Element => {
         page={page}
         offset={offset}
         handlePagination={handlePagination}
-        count={data?.total_pages}
+        count={list.data?.total_pages}
         first={1}
-        next={page === data?.total_pages ? undefined : 'exist'}
+        next={page === list.data?.total_pages ? undefined : 'exist'}
       />
     </>
   );
