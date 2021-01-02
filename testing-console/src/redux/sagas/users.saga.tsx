ac6f6fb184504087c0c23d6774825b5a8492bdb9
PieroProcessing <piero.dauber@gmail.com>
2021-01-02 14:31:40 +0100

    implementing saga

---
+++ testing-console/src/redux/sagas/users.saga.tsx
@@ -0,0 +1,42 @@
+import { call, CallEffect, ForkEffect, put, PutEffect, takeEvery } from 'redux-saga/effects';
+import { JsonPlaceholderModel } from '../../models';
+import type from '../types';
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+const getApi = (): Promise<JsonPlaceholderModel> => {
+  return fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res): Promise<JsonPlaceholderModel> => (await res.json()) as JsonPlaceholderModel)
+    .catch((err) => {
+      throw err;
+    });
+};
+
+interface ErrorModel {
+  message: string;
+}
+/* eslint-disable */
+type FetchGeneratorModel = Generator<
+  CallEffect<JsonPlaceholderModel> | PutEffect<{ type: string; users: JsonPlaceholderModel }> | PutEffect<{ type: string; message: string }>,
+  void,
+  unknown
+>;
+function* fetchUsers(): FetchGeneratorModel {
+  try {
+    const users = yield call(getApi);
+    yield put({ type: type.GET_USERS_SUCCESS, users: users as JsonPlaceholderModel });
+  } catch (error) {
+    yield put({ type: type.GET_USERS_FAILED, message: (error as ErrorModel).message });
+  }
+}
+
+function* userSaga(): Generator<ForkEffect<{ type: string }>, void, unknown> {
+  yield takeEvery(type.GET_USERS_REQUESTED, fetchUsers);
+}
+
+export default userSaga;
--- testing-console/src/redux/store.tsx
+++ testing-console/src/redux/store.tsx
@@ -1,5 +1,14 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
+import rootSaga from './sagas';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+const composedEnhancer = composeWithDevTools(applyMiddleware(sagaMiddleware));
+
+const store = createStore(rootReducer, composedEnhancer);
+
+sagaMiddleware.run(rootSaga);
+
 export default store;
