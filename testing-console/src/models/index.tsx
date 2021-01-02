d86883198ccad8eecf3f3d37294f351e0907463b
PieroProcessing <piero.dauber@gmail.com>
2021-01-02 14:32:26 +0100

    new models for saga testing

--- testing-console/src/models/index.tsx
+++ testing-console/src/models/index.tsx
@@ -1,6 +1,6 @@
 import { PokemonResponse } from './pokemon';
 import { RequestResponse, RequestData, PostResponseModel, PostParams, UrlQueryParamsModel } from './request';
-
+import { JsonPlaceholderModel } from './jsonplaceholder';
 type Response = PokemonResponse | RequestResponse;
 
-export type { Response, PokemonResponse, RequestResponse, RequestData, PostResponseModel, PostParams, UrlQueryParamsModel };
+export type { Response, PokemonResponse, RequestResponse, RequestData, PostResponseModel, PostParams, UrlQueryParamsModel, JsonPlaceholderModel };
