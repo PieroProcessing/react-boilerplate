655f3bee100a63f13f42223dee8c230c7009141a
PieroProcessing <piero.dauber@gmail.com>
2021-04-19 20:52:04 +0200

    redux toolkit

--- testing-console/src/redux/sagas/index.tsx
+++ testing-console/src/redux/sagas/index.tsx
@@ -1,6 +1,6 @@
 import { all, AllEffect, ForkEffect } from '@redux-saga/core/effects';
-import userSaga from './users.saga';
+// import userSaga from './users.saga';
 
 export default function* rootSaga(): Generator<AllEffect<Generator<ForkEffect<{ type: string }>, void, unknown>>> {
-  yield all([userSaga()]);
+  yield all([]);
 }
